d22f7d189e46ba217ad53cbd202f36433faa1b4c
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2020-10-17 15:51:05 -0300

    Configuring to Deploy on Heroku

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export const userKey = '__sharppen_user'
-export const baseApiUrl = 'http://localhost:3000'
+export const baseApiUrl = 'https://backend-sharppen.herokuapp.com/'
 
 export function showError(e) {
     if(e && e.response && e.response.data) {
